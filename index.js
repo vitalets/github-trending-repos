0ad56cb7fc578963b683c38f6789d63a3d7516b2
vitalets <vitalets@yandex-team.ru>
2017-11-15 12:09:04 +0300

    support zero starsAdded in comment body

--- index.js
+++ index.js
@@ -125,7 +125,7 @@ async function postComment(issue, newTrendingRepos) {
     const commentItem = [
       `[${repo.name.replace('/', ' / ')}](${repo.url})`,
       repo.description,
-      `***+${repo.starsAdded}** stars ${since}*`
+      repo.starsAdded ? `***+${repo.starsAdded}** stars ${since}*` : `*No info about stars ${since}*`
     ].filter(Boolean).join('\n');
     commentItems.push(commentItem);
   });
