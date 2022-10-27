a21e1b4749f7514d6fc7c3a7e736d3ab32796f49
Vitaliy Potapov <noginsk@rambler.ru>
2022-10-27 11:26:30 +0400

    translator: use proxy host

--- scripts/helpers/translator.js
+++ scripts/helpers/translator.js
@@ -8,6 +8,7 @@ const {logError, log} = require('./logger');
 
 const ONLY_LATIN_SYMBOLS = /^[\u0000-\u007F]*$/; // eslint-disable-line no-control-regex
 const EMOJI = emojiRegex();
+const GOOGLE_TRANSLATE_PROXY_HOST = process.env.GOOGLE_TRANSLATE_PROXY_HOST;
 
 module.exports = class Translator {
   constructor(text) {
@@ -42,13 +43,20 @@ module.exports = class Translator {
     return ONLY_LATIN_SYMBOLS.test(this._cleanText);
   }
 
-  async _translate() {
+  async _translate(host = '') {
     try {
       // this._result = await translate(this._text, {to: 'en', client: 'gtx'});
       // new translate using google-translate-api v9
-      this._result = await translate(this._text, { to: 'en' });
+      const options = { to: 'en' };
+      if (host) options.host = host;
+      this._result = await translate(this._text, options);
     } catch (e) {
-      logError(e);
+      if (!host && e.name === 'TooManyRequestsError' && GOOGLE_TRANSLATE_PROXY_HOST) {
+        log(`TooManyRequestsError. Retrying with proxy host...`);
+        await this._translate(GOOGLE_TRANSLATE_PROXY_HOST);
+      } else {
+        logError(e);
+      }
     }
   }
 };
